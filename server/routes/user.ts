e0df7b77d04c930f732f344675c9852d3691a27a
ali-gazizade <94519454+ali-gazizade@users.noreply.github.com>
2023-06-17 09:53:06 +0400

    Update user.ts

--- server/routes/user.ts
+++ server/routes/user.ts
@@ -14,35 +14,21 @@ router.get('/', async (req: Request, res: Response) => {
 });
 
 // POST /users
+// Todo add validations
 router.post('/', async (req: Request, res: Response) => {
-  const { name, email, password } = req.body;
+  const { name, username, password } = req.body;
 
   // Validate the request body
-  if (!name || !email || !password) {
-    return res.status(400).json({ error: 'Name, email, and password are required' });
+  if (!name || !username || !password) {
+    return res.status(400).json({ error: 'Name, username, and password are required' });
   }
 
   try {
-    const newUser = new UserModel({ name, email, password });
+    const newUser = new UserModel({ name, username, password });
     const savedUser = await newUser.save();
     res.json(savedUser);
   } catch (error) {
     res.status(500).json({ error: 'An error occurred while creating a user' });
-  }
-});
-
-// GET /users/:id
-router.get('/:id', async (req: Request, res: Response) => {
-  const userId = req.params.id;
-
-  try {
-    const user = await UserModel.findById(userId).exec();
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
-    }
-    res.json(user);
-  } catch (error) {
-    res.status(500).json({ error: 'An error occurred while fetching the user' });
   }
 });
 
