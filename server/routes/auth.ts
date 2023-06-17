c056b5ca147899720521d248d9f52c5263687551
ali-gazizade <94519454+ali-gazizade@users.noreply.github.com>
2023-06-17 09:47:41 +0400

    Updated auth.ts. Changed email to username

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -6,101 +6,79 @@ import UserModel from '../models/user';
 
 const router = express.Router();
 
-// Register route
-router.post(
-  '/register',
-  [
-    body('name').notEmpty().withMessage('Name is required'),
-    body('email').isEmail().withMessage('Invalid email'),
-    body('password')
-      .isLength({ min: 6 })
-      .withMessage('Password must be at least 6 characters long'),
-  ],
-  async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-
-    const { name, email, password } = req.body;
-
-    try {
-      // Check if there are any users in the database
-      const userCount = await UserModel.countDocuments();
-      if (userCount > 0) {
-        // Verify authorization token
-        const authHeader = req.headers.authorization;
-        if (!authHeader) {
-          return res.status(401).json({ message: 'Authorization token required' });
-        }
-
-        const token = authHeader.split(' ')[1];
-        try {
-          const decodedToken = jwt.verify(token, 'your-secret-key') as JwtPayload;
-          if (decodedToken.userId !== 'your-authorized-user-id') {
-            return res.status(403).json({ message: 'Unauthorized' });
-          }
-        } catch (error) {
-          return res.status(401).json({ message: 'Invalid authorization token' });
-        }
-      }
-
-      // Hash the password
-      const hashedPassword = await bcrypt.hash(password, 10);
-
-      // Create the user
-      const user = new UserModel({
-        name,
-        email,
-        password: hashedPassword,
-      });
-      await user.save();
-
-      res.json({ message: 'Registration successful' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Internal server error' });
-    }
+// Register the first user
+router.post('/register_first', [
+  body('name').notEmpty().withMessage('Name is required'),
+  body('username').isLength({ min: 3 }).withMessage('Username must be at least 3 characters long'),
+  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+],
+async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
   }
-);
+  
+  const { name, username, password } = req.body;
 
-// Login route
-router.post(
-  '/login',
-  [
-    body('email').isEmail().withMessage('Invalid email'),
-    body('password').notEmpty().withMessage('Password is required'),
-  ],
-  async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+  try {
+    // Check if there are any users in the database
+    const userCount = await UserModel.countDocuments();
+    if (userCount > 0) {
+      return res.status(401).json({ message: 'There are existing users' });
     }
+    
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    const { email, password } = req.body;
+    // Create the user
+    const user = new UserModel({
+      name,
+      username,
+      password: hashedPassword,
+    });
+    await user.save();
 
-    try {
-      // Check if user exists
-      const user = await UserModel.findOne({ email });
-      if (!user) {
-        return res.status(400).json({ message: 'User not found' });
-      }
-
-      // Compare passwords
-      const isPasswordValid = await bcrypt.compare(password, user.password);
-      if (!isPasswordValid) {
-        return res.status(400).json({ message: 'Invalid password' });
-      }
-
-      // Generate JWT token
-      const token = jwt.sign({ userId: user.id }, 'your-secret-key');
-
-      res.json({ token });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Internal server error' });
-    }
+    res.json({ message: 'Registration successful' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
   }
-);
+});
+
+// Login
+router.post('/login', [
+  body('username').notEmpty().withMessage('Username is required'),
+  body('password').notEmpty().withMessage('Password is required'),
+],
+async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { username, password } = req.body;
+
+  try {
+    // Check if user exists
+    const user = await UserModel.findOne({ username });
+    if (!user) {
+      return res.status(400).json({ message: 'User not found' });
+    }
+
+    // Compare passwords
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: 'Invalid password' });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign({ userId: user.id }, 'your-secret-key');
+
+    res.json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
 
 export default router;
